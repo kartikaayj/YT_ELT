1b2ba25ddbdb915ae104224f448e962f1ce61585
kartikaayj <kartikayjindal13@gmail.com>
2026-05-25 00:49:36 +0530

    Added .env to gitignore and saved API KEY inside the .env file

--- video_stats.py
+++ video_stats.py
@@ -1,15 +1,42 @@
 import requests
 import json
 
-API_KEY = "AIzaSyDg1O8uqYzprj96J9ohuulsONORFgTAhek"
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
 
-url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername={CHANNEL_HANDLE}&key={API_KEY}"
 
-response = requests.get(url)
+def get_playlist_id():
 
-print(response)
+    try:
 
-data = response.json()
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
-print(json.dumps(data,indent=4))
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        # print(json.dumps(data,indent=4))
+
+        channel_items = data["items"][0]
+
+        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"][
+            "uploads"
+        ]
+
+        print(channel_playlistId)
+
+        return channel_playlistId
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+
+if __name__ == "__main__":
+    playlistId = get_playlist_id()
